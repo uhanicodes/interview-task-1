55502b7c5352747c3fc2a3785b2da5f3da05acff
uhanicodes <uh.snigdha@gmail.com>
2025-06-12 16:57:27 +0600

    started the landing page.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [new URL('https://res.cloudinary.com')],
+        domains: ['res.cloudinary.com'],
+    },
+};
 
 export default nextConfig;
